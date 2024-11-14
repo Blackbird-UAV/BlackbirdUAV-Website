44a916818eb4081fb35d22962cb1c4e9c583e7b3
MarcVidalCodes <marcgavvid@gmail.com>
2024-11-14 17:21:13 -0500

    to top button change

--- website/src/components/BackToTopButton.js
+++ website/src/components/BackToTopButton.js
@@ -3,16 +3,24 @@ import styles from "@/styles/BackToTopButton.module.css";
 
 function BackToTopButton() {
   const [isVisible, setIsVisible] = useState(false);
-  const [isFooterVisible, setIsFooterVisible] = useState(false);
 
   useEffect(() => {
     const footer = document.querySelector('footer');
+    const isHomePage = window.location.pathname === '/';
+    const isMobile = window.innerWidth <= 768; // Mobile screen check
+
     const toggleVisibility = () => {
-      const footerRect = footer ? footer.getBoundingClientRect() : { top: Infinity };
-      setIsVisible(window.pageYOffset > 300 && footerRect.top > window.innerHeight);
+      // Check if the user has scrolled past 300px from the top of the page
+      if (window.scrollY > 300) {
+        setIsVisible(true); // Show button if scrolled down past 300px
+      } else {
+        setIsVisible(false); // Hide button if at the top of the page
+      }
     };
 
     window.addEventListener('scroll', toggleVisibility);
+    toggleVisibility(); // Trigger visibility on initial page load
+
     return () => window.removeEventListener('scroll', toggleVisibility);
   }, []);
 
