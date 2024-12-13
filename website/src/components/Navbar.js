584b5a3f93f4f0b6a37d8fd113df4f47434e9c6b
Daniel Lu <56236512+FinityFly@users.noreply.github.com>
2024-12-13 16:07:15 -0500

    Fixed unique key prop error

--- website/src/components/Navbar.js
+++ website/src/components/Navbar.js
@@ -1,20 +1,20 @@
-import Link from 'next/link';
-import Image from 'next/image';
-import { useRouter } from 'next/router';
-import { useState, useEffect } from 'react';
+import Link from "next/link";
+import Image from "next/image";
+import { useRouter } from "next/router";
+import { useState, useEffect } from "react";
 import styles from "@/styles/Navbar.module.css";
 import dropdownStyles from "../styles/Dropdown.module.css";
-import { Menu, Center } from '@mantine/core';
-import { IconChevronDown } from '@tabler/icons-react';
+import { Menu, Center } from "@mantine/core";
+import { IconChevronDown } from "@tabler/icons-react";
 export default function Navbar() {
   const router = useRouter();
   const [showNavbar, setShowNavbar] = useState(false);
 
   useEffect(() => {
     // Only add scroll event listener for the home page
-    if (router.pathname === '/') {
+    if (router.pathname === "/") {
       const handleScroll = () => {
-        const secondDiv = document.getElementById('secondDiv');
+        const secondDiv = document.getElementById("secondDiv");
         if (secondDiv && window.scrollY >= secondDiv.offsetTop) {
           setShowNavbar(true);
         } else {
@@ -23,33 +23,34 @@ export default function Navbar() {
       };
 
       // Attach scroll event listener
-      window.addEventListener('scroll', handleScroll);
+      window.addEventListener("scroll", handleScroll);
 
       // Cleanup on unmount
       return () => {
-        window.removeEventListener('scroll', handleScroll);
+        window.removeEventListener("scroll", handleScroll);
       };
     }
   }, [router.pathname]);
 
   // If not on the home page, ensure navbar is always visible
-  const navbarClass = router.pathname === '/' 
-    ? `${styles.navbar} ${showNavbar ? styles.show : ''}`
-    : `${styles.navbar} ${styles.show}`;
+  const navbarClass =
+    router.pathname === "/"
+      ? `${styles.navbar} ${showNavbar ? styles.show : ""}`
+      : `${styles.navbar} ${styles.show}`;
 
-    const teamLinks = [
-      { link: '/MeetTheTeam/2024-2025', label: 'Current Team' },
-      { link: '/MeetTheTeam/2023-2024', label: '2023 - 2024' },
-      { link: '/MeetTheTeam/2022-2023', label: '2022 - 2023' },
-      { link: '/MeetTheTeam/pastMembers', label: 'Past Members' },
-    ];
+  const teamLinks = [
+    { link: "/MeetTheTeam/2024-2025", label: "Current Team" },
+    { link: "/MeetTheTeam/2023-2024", label: "2023 - 2024" },
+    { link: "/MeetTheTeam/2022-2023", label: "2022 - 2023" },
+    { link: "/MeetTheTeam/pastMembers", label: "Past Members" },
+  ];
 
   return (
     <nav className={navbarClass}>
       <Link href="/">
         <div className={styles.logoContainer}>
           <Image
-            src="/logos/BirdLogo.png" 
+            src="/logos/BirdLogo.png"
             alt="Logo"
             fill
             className={styles.logo}
@@ -59,44 +60,66 @@ export default function Navbar() {
       </Link>
 
       <div className={styles.linksContainer}>
-        <div className={`${styles.linkWrapper} ${router.pathname === '/' ? styles.activeLink : ''}`}>
+        <div
+          className={`${styles.linkWrapper} ${
+            router.pathname === "/" ? styles.activeLink : ""
+          }`}
+        >
           <Link href="/" className={styles.link}>
             <span>Home</span>
           </Link>
         </div>
-        <div className={`${styles.linkWrapper} ${router.pathname === '/vehicles' ? styles.activeLink : ''}`}>
+        <div
+          className={`${styles.linkWrapper} ${
+            router.pathname === "/vehicles" ? styles.activeLink : ""
+          }`}
+        >
           <Link href="/vehicles" className={styles.link}>
             <span>Vehicles</span>
           </Link>
         </div>
 
-          <Menu trigger="hover" transitionProps={{ exitDuration: 0 }}>
-            <Menu.Target>
-              <div
-                className={`${styles.linkWrapper} ${router.pathname.startsWith('/meetTeam') ? styles.activeLink : ''}`}
-                style={{ position: 'relative' }}
-              >
-                <Center className={styles.link}>
-                  <span>
-                    Team
-                    <IconChevronDown size="0.9rem" stroke={1.5} color="#f9fafb" className={styles.chevron} />
-                  </span>
-                </Center>
-              </div>
-            </Menu.Target>
+        <Menu trigger="hover" transitionProps={{ exitDuration: 0 }}>
+          <Menu.Target>
+            <div
+              className={`${styles.linkWrapper} ${
+                router.pathname.startsWith("/meetTeam") ? styles.activeLink : ""
+              }`}
+              style={{ position: "relative" }}
+            >
+              <Center className={styles.link}>
+                <span>
+                  Team
+                  <IconChevronDown
+                    size="0.9rem"
+                    stroke={1.5}
+                    color="#f9fafb"
+                    className={styles.chevron}
+                  />
+                </span>
+              </Center>
+            </div>
+          </Menu.Target>
 
-            <Menu.Dropdown className={dropdownStyles.dropdownMenu}>
-              {teamLinks.map((item) => (
-                <Link href={item.link}>
-                  <Menu.Item key={item.link} className={dropdownStyles.dropdownItem}>
-                      {item.label}
-                  </Menu.Item>
-                </Link>
-              ))}
-            </Menu.Dropdown>
-          </Menu>
+          <Menu.Dropdown className={dropdownStyles.dropdownMenu}>
+            {teamLinks.map((item) => (
+              <Link href={item.link} key={item.link}>
+                <Menu.Item
+                  key={item.link}
+                  className={dropdownStyles.dropdownItem}
+                >
+                  {item.label}
+                </Menu.Item>
+              </Link>
+            ))}
+          </Menu.Dropdown>
+        </Menu>
 
-        <div className={`${styles.linkWrapper} ${router.pathname === '/joinTheTeam' ? styles.activeLink : ''}`}>
+        <div
+          className={`${styles.linkWrapper} ${
+            router.pathname === "/joinTheTeam" ? styles.activeLink : ""
+          }`}
+        >
           <Link href="/joinTheTeam" className={styles.link}>
             <span>Join</span>
           </Link>
