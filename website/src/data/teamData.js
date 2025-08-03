774751425a47014d5d07035e289b26c3f5df6836
Jeremy Friesen <jeremyfriesen12@gmail.com>
2025-08-03 00:57:34 -0400

    fix: remove test

--- website/src/data/teamData.js
+++ website/src/data/teamData.js
@@ -712,7 +712,6 @@ const teamData = {
           { firstName: 'Duaa', role: 'Support' },
           { firstName: 'Cole', role: 'Support' },
           { firstName: 'Lucas', role: 'Support' },
-          { firstName: 'Test', role: 'Test' },
         ]
       },
       2024: {
