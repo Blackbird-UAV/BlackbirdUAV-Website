8617e399809c2c755039769fa19224b4ca3df3b1
Jeremy Friesen <jeremyfriesen12@gmail.com>
2025-08-04 14:28:41 -0400

    test: test

--- website/src/data/teamData.js
+++ website/src/data/teamData.js
@@ -712,6 +712,7 @@ const teamData = {
           { firstName: 'Duaa', role: 'Support' },
           { firstName: 'Cole', role: 'Support' },
           { firstName: 'Lucas', role: 'Support' },
+          { firstName: 'Lucas', role: 'Test' },
         ]
       },
       2024: {
