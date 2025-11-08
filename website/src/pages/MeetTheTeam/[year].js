b876d58596d359746c3d6a1c0355bf3067dfa43d
Jeremy Friesen <jeremyfriesen12@gmail.com>
2025-11-07 19:46:41 -0500

    test: ci

--- website/src/pages/MeetTheTeam/[year].js
+++ website/src/pages/MeetTheTeam/[year].js
@@ -28,7 +28,7 @@ const MeetTeam = () => {
 
   let teamTitle
   if (year === '2024-2025') {
-    teamTitle = 'Current Team Test'
+    teamTitle = 'Current Team Tests'
   } else if (year === 'pastMembers') {
     teamTitle = 'Past Members'
   } else if (year === '2023-2024') {
